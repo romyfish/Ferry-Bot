fb1d0b59d6bbc8d13b4fb08563c18110aa58c174
romyfish <57704005+romyfish@users.noreply.github.com>
2023-08-09 18:05:36 +0100

    Deal with multi-dates to reschedule
    Checking among various time choices with users and manager

--- actions/actions.py
+++ actions/actions.py
@@ -159,6 +159,89 @@ class ActionRecordUnwill(Action):
 
         return []
     
+def identify_date(day_text):
+    digit_date = []
+    digit_date = re.findall("\d+", day_text)
+    if digit_date != []:   # if user give the specific date
+        new_day = int(digit_date[-1])
+        if new_day > tomorrow_date.day:   # compare the month
+            new_date = datetime.date(tomorrow_date.year, tomorrow_date.month, new_day)
+        else:
+            new_date = datetime.date(tomorrow_date.year, tomorrow_date.month+1, new_day)
+    elif day_text == "tommorow":
+        new_date = tomorrow_date
+    elif day_text == "day after tomorrow":
+        new_date = (datetime.datetime.now() + datetime.timedelta(days=2))
+    elif day_text in week_ls:   # if user give the weekday
+        # match the weekday with the date (assume in this week)
+        ddays = week_ls.index(day_text) - tomorrow_date.weekday()
+        new_date = (tomorrow_date + datetime.timedelta(days=ddays))
+    else:
+        # deal with other situation
+        new_date = tomorrow_date
+    return new_date
+
+def summary_info(data):
+    participants = data["events"][next_event_ID]["participants"]
+    potential_dates = data["events"][next_event_ID]["potential_date"]
+    unknown = []
+    refuse = []
+    users_change = []
+    change_time = []
+    approve_change_count = 0
+    affirm_count = 0
+    for p in participants:
+        for user in data["users"]:
+            if user["id"] == p:
+                user_name = user["name"]
+                break
+        if participants[p] == "unknown":
+            unknown.append(user_name)
+        elif participants[p] == "refuse":
+            refuse.append(user_name)
+        elif re.findall("\d+", participants[p]) != []:
+            users_change.append(user_name)
+            change_time.append(participants[p])
+        elif participants[p] == "approve_change":
+            approve_change_count += 1
+        else:
+            affirm_count += 1
+    results = ""
+    if unknown != []:
+        str_unknown = ""
+        if len(unknown) >= 2:
+            for i in range(len(unknown)-1):
+                str_unknown += unknown[i] + ", "
+            str_unknown = str_unknown[:-2] + " and " + unknown[-1]
+        else:
+            str_unknown += unknown[0]
+        results += str_unknown + " didn't reply. "
+    if refuse != []:
+        str_refuse = ""
+        if len(refuse) >= 2:
+            for i in range(len(refuse)-1):
+                str_refuse += refuse[i] + ", "
+            str_refuse = str_refuse[:-2] + " and " + refuse[-1]
+        else:
+            str_refuse += refuse[0]
+        results += str_refuse + " refused to attend. "
+    if users_change != []:
+        str_change = ""
+        for i in range(len(users_change)):
+            str_change += users_change[i] + " suggested to reschedule the event to " + change_time[i] + ". "
+        results += str_change
+        if approve_change_count > 0:
+            potential_dates = sorted(potential_dates.items())
+            str_approve_change = "It seems to have the most people ({}) agreeing to switch to {}. ".format(potential_dates[0][1], potential_dates[0][0])
+        else:
+            str_approve_change = "But it seems no one agree with that. "
+        results += str_approve_change
+    if results == "":
+        results += "Everyone affirmed to attend the event tomorrow!"
+    elif affirm_count > 0:
+        results += "And the others affirmed to attend the event tomorrow!"
+    return results
+
 class ActionRecordTime(Action):
 
     def name(self) -> Text:
@@ -168,28 +251,10 @@ class ActionRecordTime(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         day_text = tracker.get_slot("day")
+        new_date = identify_date(day_text).strftime("%Y-%m-%d")
 
-        # --- ! record the new time and ask to other users ---
-        # identify the new date
-        digit_date = []
-        digit_date = re.findall("\d+", day_text)
-        if digit_date != []:   # if user give the specific date
-            new_day = digit_date[0]
-            if int(new_day) > tomorrow_date.day:   # compare the month
-                new_date = tomorrow_date.strftime("%Y-%m-") + str(new_day).zfill(2)
-            else:
-                new_date = tomorrow_date.strftime("%Y-") + str(tomorrow_date.month+1).zfill(2) + "-" + str(new_day).zfill(2)
-        elif day_text == "tommorow":
-            new_date = tomorrow_date.strftime("%Y-%m-%d")
-        elif day_text == "day after tomorrow":
-            new_date = (datetime.datetime.now() + datetime.timedelta(days=2)).strftime("%Y-%m-%d")
-        elif day_text in week_ls:   # if user give the weekday
-            # match the weekday with the date (assume in this week)
-            ddays = week_ls.index(day_text) - tomorrow_date.weekday()
-            new_date = (tomorrow_date + datetime.timedelta(days=ddays)).strftime("%Y-%m-%d")
-        else:
-            # deal with other situation
-            new_date = "2001-01-01"
+        # --- ! record the time and ask to other users ---
+        askOther = False
         conversation_id = tracker.sender_id   # tracker.sender_id / "6490110454383266"
         rfile = open('./database.json', 'r')
         content = rfile.read()
@@ -199,13 +264,105 @@ class ActionRecordTime(Action):
             if user["chatID"] == conversation_id:
                 userID = user["id"]
                 break
-        data["events"][next_event_ID]["participants"][userID] = new_date
+        user_status = data["events"][next_event_ID]["participants"][userID]
+        if user_status == "affirm" or user_status == "deny_change":
+            data["events"][next_event_ID]["participants"][userID] = "approve_change"
+        elif user_status != "approve_change":
+            data["events"][next_event_ID]["participants"][userID] = new_date
+        potential_dates = data["events"][next_event_ID]["potential_date"]
+        if potential_dates.get(new_date) == None:
+            askOther = True
+            potential_dates[new_date] = 0
+        else:
+            potential_dates[new_date] += 1
+        data["events"][next_event_ID]["potential_date"] = potential_dates
+        new_json = json.dumps(data, indent=4)
+        wfile = open('./database.json', 'w')
+        wfile.write(new_json)
+        wfile.close()
+        text_message = "Sure! I'm checking the potential event date {} with other participants. I'll let you know when it's confirmed. Hope to see you soon!".format(new_date)
+        # dispatcher.utter_message(text="Sure! I'm checking with other participants. I'll let you know when it's confirmed. Hope to see you soon!")
+        dispatcher.utter_message(text=text_message)
+        if askOther:
+            # trigger a reminder to ask other users
+            date = datetime.datetime.now() + datetime.timedelta(seconds=5)
+            reminder = ReminderScheduled(
+                "EXTERNAL_time_change",
+                trigger_date_time=date,
+                name="event_time_change",
+                kill_on_user_message=False,
+            )
+
+            return [reminder]
+        
+        else:
+            # --- ! check whether get all the feedback about rescheduling from all other user affirmed to attend ---
+            informM = True
+            participants = data["events"][next_event_ID]["participants"]
+            for p in participants:
+                if participants[p] == "unknown" or participants[p] == "affirm":
+                    informM = False
+                    break
+            if informM:
+                message_text = "Someone asked for rescheduling tomorrow's event, and I've got all the feedback from others as follows. "
+                message_text += summary_info(data)
+                send_FBmessage(mng_psid, message_text)
+            
+            # dispatcher.utter_message(text="Sure! I'll put it on the record.")
+            return []
+
+class ActionRecordTimeAfter(Action):
+
+    def name(self) -> Text:
+        return "action_record_time_after"
+
+    def run(self, dispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        day_text = tracker.get_slot("day")
+        after_date = identify_date(day_text)
+
+        # identify the dates within this week
+        weekdayIdx = after_date.weekday()
+        dates = []
+        for i in range(weekdayIdx, 6):
+            after_date += datetime.timedelta(days=1)
+            dates.append(after_date.strftime("%Y-%m-%d"))
+        # --- ! record the new potential dates and ask to other users ---
+        conversation_id = tracker.sender_id   # tracker.sender_id / "6490110454383266"
+        rfile = open('./database.json', 'r')
+        content = rfile.read()
+        data = json.loads(content)
+        rfile.close()
+        for user in data["users"]:
+            if user["chatID"] == conversation_id:
+                userID = user["id"]
+                break
+        potential_dates = data["events"][next_event_ID]["potential_date"]
+        if len(dates) < 2:
+            str_dates = dates[0]
+            if potential_dates.get(str_dates) == None:
+                potential_dates[str_dates] = 0
+            else:
+                potential_dates[str_dates] += 1
+        else:
+            str_dates = ""
+            for i in range(len(dates)-1):
+                str_dates += dates[i] + ", "
+            str_dates = str_dates[:-2] + " or " + dates[-1]
+            for date in dates:
+                if potential_dates.get(date) == None:
+                    potential_dates[date] = 0
+                else:
+                    potential_dates[date] += 1
+        data["events"][next_event_ID]["participants"][userID] = str_dates
+        data["events"][next_event_ID]["potential_date"] = potential_dates
         new_json = json.dumps(data, indent=4)
         wfile = open('./database.json', 'w')
         wfile.write(new_json)
         wfile.close()
 
-        text_message = "Sure! I'm checking the new event date {} with other participants. I'll let you know when it's confirmed. Hope to see you soon!".format(new_date)
+        text_message = "Sure! I'm checking the potential event time on {} with other participants. I'll let you know when it's confirmed. Hope to see you soon!".format(str_dates)
         # dispatcher.utter_message(text="Sure! I'm checking with other participants. I'll let you know when it's confirmed. Hope to see you soon!")
         dispatcher.utter_message(text=text_message)
 
@@ -293,16 +450,80 @@ class ActionGetChangeAgree(Action):
             if user["chatID"] == conversation_id:
                 userID = user["id"]
                 break
+        potential_dates = data["events"][next_event_ID]["potential_date"]
+        if len(potential_dates) >= 2:
+            potential_dates = sorted(potential_dates)
+            str_dates = ""
+            for i in range(len(potential_dates)-1):
+                str_dates += potential_dates[i] + ", "
+            str_dates = str_dates[:-2] + " and " + potential_dates[-1]
+            dispatcher.utter_message(text="Please pick a day among {}.".format(str_dates))
+            return []
         data["events"][next_event_ID]["participants"][userID] = "approve_change"
+        potential_dates[list(potential_dates.keys())[0]] += 1
+        data["events"][next_event_ID]["potential_date"] = potential_dates
         new_json = json.dumps(data, indent=4)
         wfile = open('./database.json', 'w')
         wfile.write(new_json)
         wfile.close()
 
+        # --- ! check whether get all the feedback about rescheduling from all other user affirmed to attend ---
+        informM = True
+        participants = data["events"][next_event_ID]["participants"]
+        for p in participants:
+            if participants[p] == "unknown" or participants[p] == "affirm":
+                informM = False
+                break
+        if informM:
+            message_text = "Someone asked for rescheduling the event, and I've got all the feedback from others as follows. "
+            message_text += summary_info(data)
+            send_FBmessage(mng_psid, message_text)
+        
         dispatcher.utter_message(text="Sure! I'll put it on the record.")
 
         return []
     
+class ActionGetChangeDeny(Action):
+
+    def name(self) -> Text:
+        return "action_get_change_deny"
+
+    def run(self, dispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # --- ! record the user deny the time change ---
+        conversation_id = tracker.sender_id   # tracker.sender_id / "6490110454383266"
+        rfile = open('./database.json', 'r')
+        content = rfile.read()
+        data = json.loads(content)
+        rfile.close()
+        for user in data["users"]:
+            if user["chatID"] == conversation_id:
+                userID = user["id"]
+                break
+        data["events"][next_event_ID]["participants"][userID] = "deny_change"
+        new_json = json.dumps(data, indent=4)
+        wfile = open('./database.json', 'w')
+        wfile.write(new_json)
+        wfile.close()
+
+        # --- ! check whether get all the feedback about rescheduling from all other user affirmed to attend ---
+        informM = True
+        participants = data["events"][next_event_ID]["participants"]
+        for p in participants:
+            if participants[p] == "unknown" or participants[p] == "affirm":
+                informM = False
+                break
+        if informM:
+            message_text = "Someone asked for rescheduling the event, and I've got all the feedback from others as follows. "
+            message_text += summary_info(data)
+            send_FBmessage(mng_psid, message_text)
+        
+        dispatcher.utter_message(text="Got it. I'll record it.")
+
+        return []
+
 class ActionInformResult(Action):
 
     def name(self) -> Text:
@@ -319,53 +540,9 @@ class ActionInformResult(Action):
             content = rfile.read()
             data = json.loads(content)
             rfile.close()
-            participants = data["events"][next_event_ID]["participants"]   # !! express with sentence or table
-            refuse = []
-            users_change = []
-            change_time = []
-            approve_change = []
-            affirm_count = 0
-            for p in participants:
-                if participants[p] == "refuse":
-                    refuse.append(p)
-                elif re.findall("\d+", participants[p]) != []:
-                    users_change.append(p)
-                    change_time.append(participants[p])
-                elif participants[p] == "approve_change":
-                    approve_change.append(p)
-                else:
-                    affirm_count += 1
-            results = ""
-            if refuse != []:
-                str_refuse = ""
-                if len(refuse) >= 2:
-                    for i in range(len(refuse)-1):
-                        str_refuse += refuse[i] + ", "
-                    str_refuse = str_refuse[:-2] + " and " + refuse[len(refuse)-1]
-                else:
-                    str_refuse += refuse[0]
-                results += str_refuse + " refused to attend. "
-            if users_change != []:
-                str_change = ""
-                for i in range(len(users_change)):
-                    str_change += users_change[i] + " suggested to reschedule the event on " + change_time[i] + ". "
-                results += str_change
-            if approve_change != []:
-                str_approve_change = ""
-                if len(approve_change) >= 2:
-                    for i in range(len(approve_change)-1):
-                        str_approve_change += approve_change[i] + ", "
-                    str_approve_change = str_approve_change[:-2] + " and " + approve_change[len(approve_change)-1]
-                else:
-                    str_approve_change += approve_change[0]
-                results += str_approve_change + " approved to reschedule the event. "
-            else:
-                results += "no other one approved to reschedule the event."
-            if results == "":
-                results += "everyone affirmed to attend the event!"
-            elif affirm_count > 0:
-                results += "And the others affirmed to attend the event!"
-            dispatcher.utter_message(text=results)
+            event_name = data["events"][next_event_ID]["content"]
+            message = "For tomorrow's offline group {}, {}".format(event_name, summary_info(data))
+            dispatcher.utter_message(text=message)
         else:
             dispatcher.utter_message(text="I'm afraid not sure for now")
         return []
@@ -392,10 +569,9 @@ class ActionRescheduleEvent(Action):
         content = rfile.read()
         data = json.loads(content)
         rfile.close()
-        participants = data["events"][next_event_ID]["participants"]
-        for p in participants:
-            if re.findall("\d+", participants[p]) != []:
-                new_event_date = participants[p]
+        potential_dates = data["events"][next_event_ID]["potential_date"]
+        potential_dates = sorted(potential_dates.items())
+        new_event_date = potential_dates[0][0]
         data["events"][next_event_ID]["date"] = new_event_date
         new_json = json.dumps(data, indent=4)
         wfile = open('./database.json', 'w')
@@ -491,13 +667,26 @@ class ActionTimeChangeChecking(Action):
         rfile.close()
         event_name = data["events"][next_event_ID]["content"]
         participants = data["events"][next_event_ID]["participants"]
+        change_user_IDs = []
         for p in participants:
             if re.findall("\d+", participants[p]) != []:
-                change_user_ID = p
-                new_event_date = participants[p]
-        message_text="Sorry, someone has asked for rescheduling tomorrow's offline group {} for {}. Are you okay with that?".format(event_name, new_event_date)
+                change_user_IDs.append(p)
+        potential_date = data["events"][next_event_ID]["potential_date"]
+        potential_dates = []
+        for date in potential_date:
+            potential_dates.append(date)
+        if len(potential_dates) < 2:
+            new_event_date = potential_dates[0]
+            message_text="Sorry, someone has asked for rescheduling tomorrow's offline group {} to {}. Are you okay with that?".format(event_name, new_event_date)
+        else:
+            potential_dates = sorted(potential_dates)
+            str_dates = ""
+            for i in range(len(potential_dates)-1):
+                str_dates += potential_dates[i] + ", "
+            str_dates = str_dates[:-2] + " or " + potential_dates[-1]
+            message_text="Sorry, someone has asked for rescheduling tomorrow's offline group {} to {}. Are you okay with that? Could you please pick a day?".format(event_name, str_dates)
         for p in participants:
-            if p != change_user_ID and participants[p] != "refuse":
+            if p not in change_user_IDs and participants[p] != "refuse":
                 for user in data["users"]:
                     if user["id"] == p:
                         user_psid = user["chatID"]
--- test.py
+++ test.py
@@ -43,10 +43,10 @@ import datetime
 # wfile.close()
 # print(data["events"][1])
 
-# today_date = datetime.datetime.now().strftime("%Y-%m-%d")
-# print(today_date)
-# tomorrow_date = datetime.datetime.now() + datetime.timedelta(days=1)
-# print(tomorrow_date.strftime("%Y-%m-%d"))
+today_date = datetime.datetime.now().strftime("%Y-%m-%d")
+print(today_date)
+tomorrow_date = datetime.datetime.now() + datetime.timedelta(days=1)
+print(tomorrow_date.strftime("%Y-%m-%d"))
 
 # conversation_id = "6110406512402871"
 # rfile = open('./database.json', 'r')
@@ -75,7 +75,7 @@ import re
 # a = re.findall("\d+", test)
 # print(a)
 
-# week_ls = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+week_ls = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 # print(week_ls[datetime.date(2023, 7, 12).weekday()])
 
@@ -137,3 +137,120 @@ import re
 #         new_event_date = participants[rp]
 #         print(change_user_ID, new_event_date)
 
+rfile = open('./database.json', 'r')
+content = rfile.read()
+data = json.loads(content)
+rfile.close()
+potential_dates = data["events"][1]["potential_date"]
+print(list(potential_dates.keys()))
+potential_dates[list(potential_dates.keys())[0]] += 1
+print(potential_dates)
+# potential_dates = sorted(potential_dates.items())
+# print(potential_dates[0][0])
+# print(potential_dates[0][1])
+# print(potential_dates.get("2023-08-14") == None)
+# data["events"][1]["potential_date"] = potential_dates
+# new_json = json.dumps(data, indent=4)
+# wfile = open('./database.json', 'w')
+# wfile.write(new_json)
+# wfile.close()
+
+# def identify_date(day_text):
+#     digit_date = []
+#     digit_date = re.findall("\d+", day_text)
+#     if digit_date != []:   # if user give the specific date
+#         new_day = int(digit_date[0])
+#         if new_day > tomorrow_date.day:   # compare the month
+#             # new_date = tomorrow_date.strftime("%Y-%m-") + str(new_day).zfill(2)
+#             new_date = datetime.date(tomorrow_date.year, tomorrow_date.month, new_day)
+#         else:
+#             # new_date = tomorrow_date.strftime("%Y-") + str(tomorrow_date.month+1).zfill(2) + "-" + str(new_day).zfill(2)
+#             new_date = datetime.date(tomorrow_date.year, tomorrow_date.month+1, new_day)
+#     elif day_text == "tommorow":
+#         new_date = tomorrow_date
+#     elif day_text == "day after tomorrow":
+#         new_date = (datetime.datetime.now() + datetime.timedelta(days=2))
+#     elif day_text in week_ls:   # if user give the weekday
+#         # match the weekday with the date (assume in this week)
+#         ddays = week_ls.index(day_text) - tomorrow_date.weekday()
+#         new_date = (tomorrow_date + datetime.timedelta(days=ddays))
+#     else:
+#         # deal with other situation
+#         new_date = tomorrow_date
+#     return new_date
+
+# new_date = identify_date("12th")
+# print(new_date.weekday())
+# weekdayIdx = new_date.weekday()
+# dates = []
+# for i in range(weekdayIdx, 6):
+#     new_date += datetime.timedelta(days=1)
+#     dates.append(new_date.strftime("%Y-%m-%d"))
+# print(dates)
+
+# def summary_info(data):
+#     participants = data["events"][0]["participants"]
+#     unknown = []
+#     refuse = []
+#     users_change = []
+#     change_time = []
+#     approve_change = []
+#     affirm_count = 0
+#     for p in participants:
+#         if participants[p] == "unknown":
+#             unknown.append(p)
+#         elif participants[p] == "refuse":
+#             refuse.append(p)
+#         elif re.findall("\d+", participants[p]) != []:
+#             users_change.append(p)
+#             change_time.append(participants[p])
+#         elif participants[p] == "approve_change":
+#             approve_change.append(p)
+#         else:
+#             affirm_count += 1
+#     results = ""
+#     if unknown != []:
+#         str_unknown = ""
+#         if len(unknown) >= 2:
+#             for i in range(len(unknown)-1):
+#                 str_unknown += unknown[i] + ", "
+#             str_unknown = str_unknown[:-2] + " and " + unknown[-1]
+#         else:
+#             str_unknown += unknown[0]
+#         results += str_unknown + " didn't reply. "
+#     if refuse != []:
+#         str_refuse = ""
+#         if len(refuse) >= 2:
+#             for i in range(len(refuse)-1):
+#                 str_refuse += refuse[i] + ", "
+#             str_refuse = str_refuse[:-2] + " and " + refuse[-1]
+#         else:
+#             str_refuse += refuse[0]
+#         results += str_refuse + " refused to attend. "
+#     if users_change != []:
+#         str_change = ""
+#         for i in range(len(users_change)):
+#             str_change += users_change[i] + " suggested to reschedule the event on " + change_time[i] + ". "
+#         results += str_change
+#     if approve_change != []:
+#         str_approve_change = ""
+#         if len(approve_change) >= 2:
+#             for i in range(len(approve_change)-1):
+#                 str_approve_change += approve_change[i] + ", "
+#             str_approve_change = str_approve_change[:-2] + " and " + approve_change[-1]
+#         else:
+#             str_approve_change += approve_change[0]
+#         results += str_approve_change + " approved to reschedule the event. "
+#     if results == "":
+#         results += "everyone affirmed to attend the event!"
+#     elif affirm_count > 0:
+#         results += "And the others affirmed to attend the event!"
+#     return results
+
+# rfile = open('./database.json', 'r')
+# content = rfile.read()
+# data = json.loads(content)
+# rfile.close()
+# message = summary_info(data)
+# print(type(message))
+# print(message)
