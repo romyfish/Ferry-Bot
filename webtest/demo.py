139b74f5026da44a5d035b238fb446f5f9159d96
romyfish <57704005+romyfish@users.noreply.github.com>
2024-06-04 12:42:12 +0100

    Update contents

--- webtest/demo.py
+++ webtest/demo.py
@@ -15,18 +15,24 @@ for i in range(mode_count):
 # chat_prompt = [SystemMessage(content = sys_prompts[mode_num-1])]
 
 mode_descriptions = []
-mode_descriptions.append("Check the user's status")
-mode_descriptions.append("Advice for group selection")
-mode_descriptions.append("Training to be supportive")
-mode_descriptions.append("Group scenario: Schedule a meeting")
-mode_descriptions.append("Group scenario: Caution on inappropriate behaviour")
-mode_descriptions.append("Group → one-to-one: Check the user's status")
+mode_descriptions.append("Check in with Inactive Users on Their Wellbeing")
+mode_descriptions.append("Train Peer Supporters on Active Member Engagement")
+mode_descriptions.append("Match Newcomers with Suitable Peer Support Groups")
+mode_descriptions.append("Coordinate Group Availability and Preferences for Upcoming Events")
+mode_descriptions.append("Train Peer Supporters on Appropriate Language and Behaviour in Group Chats")
+
+mode_hints = []
+mode_hints.append("In this conversation, you need to pretend that you are a member of a peer support group and for some reason haven't responded to Ferrybot and bubbled in the group chat for a while. You may not want to talk to real people about your troubles but may be willing to open up when a chatbot reaches out to ask.")
+mode_hints.append("In this conversation, you need to pretend that you are in the role of a supporter of a peer support group and have some confusion about how to do a good job. Through the communication with Ferrybot, you will get guidance be well-equipped to provide valuable support and foster a welcoming community atmosphere.")
+mode_hints.append("In this conversation, you need to pretend that you are a new member of a peer support organisation in Glasgow that has various groups affiliate. You would like to join a group that is most suitable for you based on your personal characteristics (e.g. demographics and interests).")
+mode_hints.append("In this group chat conversation, you need to pretend that you are one of the members of a peer support group. This group usually arranges a weekly offline or online activity such as coffee chat, hiking, sports, picnic, crafting and gardening. You can talk freely about your availability and wanted activities for the next week, providing the conditions for Ferrybot to help determine the activities.")
+mode_hints.append("In this group chat conversation, you need to pretend that you are one of the members of a peer support group.")
 
 start_bot_texts = []
 start_bot_texts.append("Hi! I notice you haven't been active lately and want to check in. How's it going these days?")
-start_bot_texts.append("Hi newcomer! I'm Ferrybot, an intelligent chatbot for you to find a suitable peer support group in Glasgow!")
 start_bot_texts.append("Hi! I'm Ferrybot, an intelligent chatbot for you to better cope with others in a peer support group. Ask me anything or simply prompt me to give some guidance for you!")
-start_bot_texts.append("Hello everyone! We’re planning a fun offline event for our group and would love to find a time that works for everyone. Could you please share when you are generally free over the next week?")
+start_bot_texts.append("Hi newcomer! I'm Ferrybot, an intelligent chatbot for you to find a suitable peer support group in Glasgow!")
+start_bot_texts.append("Hello everyone! We're planning a fun offline event for our group and would love to find a time that works for everyone. Could you please share when you are generally free over the next week?")
 
 other_user_texts = []
 other_user_texts.append("I'm okay with Wednesday and Thursday")
@@ -46,25 +52,31 @@ def home():
 @app.route('/check_status')
 def check_status():
     session['modeNum'] = 1
-    return render_template("private.html", start_bot_text=start_bot_texts[mode_num-1], mode_num=str(mode_num), mode_description=mode_descriptions[mode_num-1])
+    return render_template("private.html", start_bot_text=start_bot_texts[mode_num-1], mode_num=str(mode_num), mode_description=mode_descriptions[mode_num-1], hint_text=mode_hints[mode_num-1], next_text=mode_descriptions[mode_num], next_url='support_train')
 
 @app.route('/ask_group_advice')
 def ask_group_advice():
-    session['modeNum'] = 2
+    session['modeNum'] = 3
     mode_num = session.get('modeNum', 1)
-    return render_template("private.html", start_bot_text=start_bot_texts[mode_num-1], mode_num=str(mode_num), mode_description=mode_descriptions[mode_num-1])
+    return render_template("private.html", start_bot_text=start_bot_texts[mode_num-1], mode_num=str(mode_num), mode_description=mode_descriptions[mode_num-1], hint_text=mode_hints[mode_num-1], next_text=mode_descriptions[mode_num], next_url='schedule')
 
 @app.route('/support_train')
 def support_train():
-    session['modeNum'] = 3
+    session['modeNum'] = 2
     mode_num = session.get('modeNum', 1)
-    return render_template("private.html", start_bot_text=start_bot_texts[mode_num-1], mode_num=str(mode_num), mode_description=mode_descriptions[mode_num-1])
+    return render_template("private.html", start_bot_text=start_bot_texts[mode_num-1], mode_num=str(mode_num), mode_description=mode_descriptions[mode_num-1], hint_text=mode_hints[mode_num-1], next_text=mode_descriptions[mode_num], next_url='ask_group_advice')
 
 @app.route('/schedule')
 def schedule():
     session['modeNum'] = 4
     mode_num = session.get('modeNum', 1)
-    return render_template("group.html", start_bot_text=start_bot_texts[mode_num-1], mode_num=str(mode_num), mode_description=mode_descriptions[mode_num-1], start_userA_text=other_user_texts[0], start_userB_text=other_user_texts[1], start_userC_text=other_user_texts[2])
+    return render_template("group.html", start_bot_text=start_bot_texts[mode_num-1], mode_num=str(mode_num), mode_description=mode_descriptions[mode_num-1], start_userA_text=other_user_texts[0], start_userB_text=other_user_texts[1], start_userC_text=other_user_texts[2], hint_text=mode_hints[mode_num-1], next_text=mode_descriptions[mode_num], next_url='train_in_group')
+
+@app.route('/train_in_group')
+def train_in_group():
+    session['modeNum'] = 5
+    mode_num = session.get('modeNum', 1)
+    return render_template("group.html", start_bot_text=start_bot_texts[mode_num-1], mode_num=str(mode_num), mode_description=mode_descriptions[mode_num-1], start_userA_text=other_user_texts[0], start_userB_text=other_user_texts[1], start_userC_text=other_user_texts[2], hint_text=mode_hints[mode_num-1])
 
 @app.route('/contact')
 def contact():
